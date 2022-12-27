691ea53129f4dec121abf03af1f6fe60221c4264
NirbanPal <paulnirban007@gmail.com>
2022-12-27 23:09:18 +0530

    bugs chart js and income crud left

--- expensewebsite/expense/urls.py
+++ expensewebsite/expense/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.views.decorators.csrf import csrf_exempt
 
 
 
@@ -8,5 +9,6 @@ urlpatterns = [
     path('addexpense',views.add_expense,name='addexpense'),
     path('edit-expense/<int:id>', views.expense_edit, name="expense-edit"),
     path('expense-delete/<int:id>', views.delete_expense, name="expense-delete"),
+    path('search-expenses', csrf_exempt(views.search_expenses),name="search_expenses"),
 
 ]
--- expensewebsite/expense/views.py
+++ expensewebsite/expense/views.py
@@ -7,22 +7,34 @@ from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 import json
 from django.http import JsonResponse
-from userpreferences.models import UserPreference
+# from userpreferences.models import UserPreference
 import datetime
 # Create your views here.
+
+
+def search_expenses(request):
+    if request.method == 'POST':
+        search_str = json.loads(request.body).get('searchText')
+        expenses = Expense.objects.filter(
+            amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            date__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            description__icontains=search_str, owner=request.user) | Expense.objects.filter(
+            category__icontains=search_str, owner=request.user)
+        data = expenses.values()
+        return JsonResponse(list(data), safe=False)
 
 
 @login_required(login_url='/authentication/login')
 def index(request):
     categories = Category.objects.all()
     expenses = Expense.objects.filter(owner=request.user)
-    # paginator = Paginator(expenses, 5)
-    # page_number = request.GET.get('page')
-    # page_obj = Paginator.get_page(paginator, page_number)
+    paginator = Paginator(expenses, 5)
+    page_number = request.GET.get('page')
+    page_obj = Paginator.get_page(paginator, page_number)
     # currency = UserPreference.objects.get(user=request.user).currency
     context = {
         'expenses': expenses,
-        # 'page_obj': page_obj,
+        'page_obj': page_obj,
         # 'currency': currency
     }
     return render(request, 'expense/index.html',context)
